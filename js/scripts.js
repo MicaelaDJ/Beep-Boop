d3086d2875d99752057ec511055a2e834342f61d
Micaela Jawor <micaelajawor@yahoo.com>
2019-01-25 11:47:04 -0800

    update js so hopefully number entered doesn't return NaN

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,30 @@
 // Business Logic
-var beepBoop = function(number) {
-  if (number === 0){
-    return "Beep!";
-  }
-  else if (number === 1) {
-    return "Boop!";
-  }
-  else if (number % 3 === 0) {
-    return "I'm sorry, Dave. I'm afraid I can't do that.";
-  }
-  else {
-    return false;
+var beepBoop = function(userInput) {
+  var results = [];
+  for (var number = 0; number <= userInput; number++) {
+    var inputNumber = (number).toString();
+    if (inputNumber === 0){
+      results.push("Beep!");
+    }
+    else if (inputNumber === 1) {
+      results.push("Boop!");
+    }
+    else if (number % 3 === 0) {
+      results.push("I'm sorry, Dave. I'm afraid I can't do that.");
+    }
+    else {
+      results.push(number + ".");
+    };
   };
+  return results
 };
 // User Interface
 $(document).ready(function() {
   $("form#robotForm").submit(function(event) {
     event.preventDefault();
-    var number = parseInt($("userNumber").val());
-    var result = beepBoop(number);
+    var userInput = parseInt($("userNumber").val());
+    var output = beepBoop(userInput);
+    var stringOutput = output.join("");
     $(".number").text(number);
     $("#result").show();
   });
