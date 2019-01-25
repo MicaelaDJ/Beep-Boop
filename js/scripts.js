7d5748235c6f9c0fc9346370783ec728b37cfa2a
Micaela Jawor <micaelajawor@yahoo.com>
2019-01-25 13:19:14 -0800

    take steps to get results to show. Not working yet

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,33 @@
 // Business Logic
 var beepBoop = function(userInput) {
   var results = [];
-  for (var number = 0; number <= userInput; number++) {
-    var inputNumber = (number).toString();
-    if (inputNumber === 0){
+  for (var index = 0; index <= userInput; index++) {
+    var inputNumber = (index).toString();
+    if (inputNumber.includes("0")){
       results.push("Beep!");
     }
-    else if (inputNumber === 1) {
+    else if (inputNumber.includes("1")) {
       results.push("Boop!");
     }
-    else if (number % 3 === 0) {
+    else if (index % 3 === 0 && index !== 0) {
       results.push("I'm sorry, Dave. I'm afraid I can't do that.");
     }
     else {
       results.push(number + ".");
-    };
-  };
+    }
+  }
   return results
-};
+}
+
 // User Interface
 $(document).ready(function() {
   $("form#robotForm").submit(function(event) {
     event.preventDefault();
-    var userInput = parseInt($("userNumber").val());
+
+    var userInput = parseInt($("inputNumber").val());
     var output = beepBoop(userInput);
     var stringOutput = output.join("");
-    $(".number").text(number);
+    $("#result").hide();
     $("#result").show();
   });
 });
