37e767c4a338f18320b355622116ad2f617c2783
Micaela Jawor <micaelajawor@yahoo.com>
2019-01-25 15:55:15 -0800

    update js and index, webpage does the basic function

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,32 @@
-// Business Logic
-var beepBoop = function(userInput) {
+//
+var beepBoop = function(userInput){
   var results = [];
   for (var index = 0; index <= userInput; index++) {
-    var inputNumber = (index).toString();
-    if (inputNumber.includes("0")){
-      results.push("Beep!");
-    }
-    else if (inputNumber.includes("1")) {
-      results.push("Boop!");
-    }
-    else if (index % 3 === 0 && index !== 0) {
+    var inputString = (index).toString();
+    if (index % 3 === 0 && index != 0) {
       results.push("I'm sorry, Dave. I'm afraid I can't do that.");
-    }
-    else {
-      results.push(number + ".");
+    } else if (inputString.includes("1")) {
+      results.push("Boop!");
+    } else if (inputString.includes("0")) {
+      results.push("Beep!");
+    } else {
+      results.push(index + ".");
     }
   }
   return results
 }
 
-// User Interface
-$(document).ready(function() {
-  $("form#robotForm").submit(function(event) {
+$(document).ready(function(){
+  $("form#robotForm").submit(function(event){
     event.preventDefault();
-
-    var userInput = parseInt($("inputNumber").val());
+    var name = $("input#name").val();
+    var userInput = parseInt($("#inputNumber").val());
     var output = beepBoop(userInput);
     var stringOutput = output.join("");
+    // var outputName = stringOutput.replace(/Dave/g, name)
     $("#result").hide();
-    $("#result").show();
+    $("#result").slideDown(1000);
+    $("#result").text(stringOutput);
+    // $('html, body').animate({ scrollTop: 0 }, 'slow');
   });
 });
